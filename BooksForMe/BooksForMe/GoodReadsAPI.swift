9ad9a9d4632666e2de4720f40921851186c613db
EduardoVaca <vacalalo@gmail.com>
2016-10-17 13:23:29 +0300

    Creating Xcode project for app

---
+++ BooksForMe/BooksForMe/GoodReadsAPI.swift
@@ -0,0 +1,48 @@
+//
+//  GoodReadsAPI.swift
+//  BooksForMe
+//
+//  Created by Eduardo Vaca on 17/10/16.
+//  Copyright © 2016 Vaca. All rights reserved.
+//
+
+import Foundation
+
+enum Method: String {
+    case SearchBooks = "/search/index.xml"
+}
+
+struct GoodReadsAPI {
+    
+    private static let baseURLString = "http://www.goodreads.com"
+    private static let apiKey = "uv1J3LcJ7zGuhzCXwaCcUQ"
+    
+    
+    private static func GoodReadsURL(method: Method, parameters: [String: String]?) -> URL {
+        
+        let methodURL = baseURLString + method.rawValue
+        
+        var components = URLComponents(string: methodURL)!
+        
+        var queryItems = [URLQueryItem]()
+        
+        let baseParameters = ["key": apiKey]
+        
+        for (key, value) in baseParameters {
+            let item = URLQueryItem(name: key, value: value)
+            queryItems.append(item)
+        }
+        
+        if let additionalParameters = parameters {
+            for (key, value) in additionalParameters {
+                let item = URLQueryItem(name: key, value: value)
+                queryItems.append(item)
+            }
+        }
+        
+        components.queryItems = queryItems
+        
+        return components.url!
+    }
+    
+}
---
+++ BooksForMe/BooksForMe/SearchBooksViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SearchBooksViewController.swift
+//  BooksForMe
+//
+//  Created by Eduardo Vaca on 17/10/16.
+//  Copyright © 2016 Vaca. All rights reserved.
+//
+
+import UIKit
+
+class SearchBooksViewController: UIViewController {
+    
+}
